8751abb969dce3abae04f2723924d6f80a94ad4a
Christian Stuart <christian.jac.stuart@gmail.com>
2018-10-12 16:51:38 +0200

    Usage tracking

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -3,14 +3,14 @@ const URL = 'https://178.128.252.36/api/';
 
 chrome.runtime.sendMessage({ type: "INJECT_STYLE" });
 
-chrome.storage.local.get('uuid', ({uuid}) => {
+chrome.storage.local.get(['uuid', 'enabled'], ({uuid, enabled}) => {
     if(uuid == null){
         const uuid_bytes = crypto.getRandomValues(new Uint8Array(16));
         uuid = Array.from(uuid_bytes).map(x => x.toString(16).padStart(2, '0')).join("");
         chrome.storage.local.set({uuid});
     }
 
-    fetch(URL + `?uuid=${uuid}`, {mode: "cors", method: "GET", credentials: "include"})
+    fetch(URL + `?uuid=${uuid}&enabled=${enabled}`, {mode: "cors", method: "GET", credentials: "include"})
         .then(response => response.text())
         .then(css => {
             chrome.runtime.sendMessage({
