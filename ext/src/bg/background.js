185c3f3e779fd6239c5ce22de74da0d046b48eb0
Christian <christian.jac.stuart@gmail.com>
2018-10-12 21:50:13 +0200

    Cache per host. Resolve some errors.

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -18,45 +18,55 @@ async function toggleEnabled(){
     return enabled;
 }
 
-function setCss(css){
+function csskey(host){
+    return `css_${host}`;
+}
+
+function setCss(css, host){
     return new Promise(resolve => {
-        chrome.storage.local.set({css}, () => resolve());
+        chrome.storage.local.set({[csskey(host)]: css}, () => resolve());
     })
 }
 
-function getCss(){
+function getCss(host){
     return new Promise(resolve => {
-        chrome.storage.local.get(["css"], r => resolve(r.css));
+        chrome.storage.local.get([csskey(host)], r => resolve(r[csskey(host)]));
+    });
+}
+
+function injectCSS(tab, css){
+    return new Promise (resolve => {
+        chrome.tabs.insertCSS(tab, {code: css}, () => { resolve(true); });
     });
 }
 
 async function handleMessage(request, sender, sendResponse){
     switch(request.type) {
-        case 'UPDATE_STYLE': 
-            await setCss(request.content);
+        case 'UPDATE_STYLE':
+            await setCss(request.content, request.host);
             return true;
+
         case 'INJECT_STYLE':
             const enabled = await getEnabled();
             const tab = sender.tab.id;
-            const css = await getCss();
-            if(enabled) {
-                chrome.tabs.insertCSS(tab, {code: css}, () => {sendResponse(true)});
+            const css = await getCss(request.host);
+            console.log(css);
+            if(css != null && enabled) {
+                return await injectCSS(tab, css);
             }
-            else {
-                sendResponse(false);
-            }
-            break;
+            return false;
+
         case 'TOGGLE_STYLE':
             const response = await toggleEnabled();
-            sendResponse(response);
-            break;
+            return response;
     }
 }
 
 //example of using a message handler from the inject scripts
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        handleMessage(request, sender, sendResponse);
+        handleMessage(request, sender).then(response => { sendResponse(response); });
         return true;
     }
 );
+
--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,26 +1,37 @@
 
 const URL = 'https://vdzijden.com/api/';
 
-chrome.runtime.sendMessage({ type: "INJECT_STYLE" });
+const host = window.location.host;
 
-chrome.storage.local.get(['uuid', 'enabled'], ({uuid, enabled}) => {
+
+function sendMessage(message){
+    return new Promise(resolve => {
+        chrome.runtime.sendMessage(message, resolve);
+    });
+}
+
+
+chrome.runtime.sendMessage({ type: "INJECT_STYLE", host }, e => console.debug(e));
+
+chrome.storage.local.get(['uuid', 'enabled'], async ({uuid, enabled}) => {
     if(uuid == null){
         const uuid_bytes = crypto.getRandomValues(new Uint8Array(16));
         uuid = Array.from(uuid_bytes).map(x => x.toString(16).padStart(2, '0')).join("");
         chrome.storage.local.set({uuid});
     }
 
-    fetch(URL + `?uuid=${uuid}&enabled=${enabled}&host=${window.location.host}`, {mode: "cors", method: "GET", credentials: "include"})
-        .then(response => response.text())
-        .then(css => {
-            chrome.runtime.sendMessage({
-                type: 'UPDATE_STYLE', 
-                content: css
-            }, css => {
-                chrome.runtime.sendMessage({type: 'INJECT_STYLE'});
-            });
-        });
+    let response = await fetch(URL + `?uuid=${uuid}&enabled=${enabled}&host=${host}`, {
+        mode: "cors",
+        method: "GET",
+        credentials: "include"
+    });
+    let css = await response.text();
 
-
+    await sendMessage({
+        type: 'UPDATE_STYLE',
+        content: css,
+        host
+    });
+    await sendMessage({type: 'INJECT_STYLE', host});
 });
 
